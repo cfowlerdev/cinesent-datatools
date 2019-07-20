c1513893e46bda2ab77f69a3ba3bfddbe8e149bf
Christian Fowler <cfowlerdev@gmail.com>
2019-07-20 19:57:02 +0800

    Fixed typos in setup script

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encodings='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -13,7 +13,7 @@ setup(
     version='0.0.1',
     description='Tools for data cleaning and preparation for analysis',
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     author='Christian Fowler',
     url='https://github.com/cfowlerdev/cinesent-datatools',
     packages=find_packages(exclude=('tests', 'docs')),
