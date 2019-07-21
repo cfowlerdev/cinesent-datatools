0c363a75807c10c71a58d2917bb67456baab311b
Christian Fowler <cfowlerdev@gmail.com>
2019-07-21 20:34:30 +0800

    Added install requirements to setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,18 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='datatools',
-    version='0.0.1',
+    name='cinesent_datatools',
+    version='0.0.2',
     description='Tools for data cleaning and preparation for analysis',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Christian Fowler',
     url='https://github.com/cfowlerdev/cinesent-datatools',
     packages=find_packages(exclude=('tests', 'docs')),
-    python_requires='>=2.7,<4'
+    python_requires='>=2.7,<4',
+    install_requires=[
+        'nltk>=3.4.4',
+        'beautifulsoup4>=4.7.1',
+        'tweet-preprocessor>=0.5.0'
+    ]
 )
